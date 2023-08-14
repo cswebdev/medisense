3f12b445fe9947bd5e81277b06cc8e30eb579543
cswebdev <csnider91@live.com>
2023-08-13 20:37:05 -0400

    initial commit

---
+++ src/main/java/com/medisense/medisense/MedisenseApplication.java
@@ -0,0 +1,13 @@
+package com.medisense.medisense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisenseApplication.class, args);
+	}
+
+}
