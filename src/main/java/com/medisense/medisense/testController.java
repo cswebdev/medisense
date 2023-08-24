367a54cff46ae175dc222a7559891580feb5f5ca
cswebdev <csnider91@live.com>
2023-08-24 18:53:22 -0400

    added lombok to project dependencies

---
+++ src/main/java/com/medisense/medisense/testController.java
@@ -0,0 +1,14 @@
+package com.medisense.medisense;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+	@RequestMapping("/api")
+	public String HelloWorld() {
+		return "hello world";
+	}
+	
+}
