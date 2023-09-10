5d3c7f804e7fcebdd2275e72cd9757cbdf74accd
cswebdev <csnider91@live.com>
2023-09-10 16:39:52 -0400

    added User getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,10 +1,18 @@
 package models;
 
 import java.util.HashSet;
+import java.util.Set;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -20,6 +28,14 @@ public class User {
     @NotBlank
     @Size(max = 50)
     private String email;
+    
+    @NotBlank
+    @Size(max = 50)
+    private String first_name;
+    
+    @NotBlank
+    @Size(max = 50)
+    private String last_name;
 
     @NotBlank
     @Size(max = 100)
@@ -33,10 +49,60 @@ public class User {
 
     }
 
-    public User(String email, String password) {
+    public User(String email, String password, String first_name, String last_name) {
         this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.password = password;
     }
 
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+    
 }
