6248aa011861e4a44092331fb15166d0744c1100
cswebdev <csnider91@live.com>
2023-09-10 16:37:36 -0400

    re-uploaded application.properties and created models package with ERole, Role, and Users classes

---
+++ src/main/java/models/ERole.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_Moderator,
+    ROLE_ADMIN
+
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.HashSet;
+
+import jakarta.persistence.*;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+}
