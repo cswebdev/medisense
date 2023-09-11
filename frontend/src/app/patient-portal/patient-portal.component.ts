42f5ce70a9ba7bc95be19d7e25a32867a2f55a50
MattTuccillo <matthew.tuccillo96@gmail.com>
2023-09-11 15:45:39 -0400

    added edit btn functionality on portal

--- frontend/src/app/patient-portal/patient-portal.component.ts
+++ frontend/src/app/patient-portal/patient-portal.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./patient-portal.component.css']
 })
 export class PatientPortalComponent {
+  public editToggled: boolean = false;
+  public firstNameDisabled: boolean = true;
+  public lastNameDisabled: boolean = true;
+  public emailDisabled: boolean = true;
+  public passwordDisabled: boolean = true;
+  public saveDisabled: boolean = true;
 
-}
+  constructor(){}
+
+  editToggle(){
+    this.firstNameDisabled = this.editToggled;
+    this.lastNameDisabled = this.editToggled;
+    this.emailDisabled = this.editToggled;
+    this.passwordDisabled = this.editToggled;
+    this.saveDisabled = this.editToggled;
+    this.editToggled = !this.editToggled;
+  }
+ }
