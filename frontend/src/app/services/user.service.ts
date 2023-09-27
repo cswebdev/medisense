2c92e76fb0f4145c0a776a84fdb9e7a7adcf5bb1
MattTuccillo <matthew.tuccillo96@gmail.com>
2023-09-26 22:05:55 -0400

    edit profile works

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 
 const baseUrl = 'http://localhost:8080/api/v1/users'
@@ -13,23 +15,30 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<User[]> {
+  getAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(baseUrl);
   }
 
-  get(id: any): Observable<User> {
+  getUser(id: any): Observable<User> {
     return this.http.get(`${baseUrl}/${id}`);
   }
 
-  create(data: any): Observable<any> {
+  createUser(data: any): Observable<any> {
     return this.http.post(baseUrl, data);
   }
 
-  update(id: any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/${id}`, data);
+  updateUser(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data).pipe(
+      catchError((error) => {
+        // Handle the error here
+        console.error('Error updating user:', error);
+        // Optionally, rethrow the error to propagate it to the component
+        return throwError(error);
+      })
+    );
   }
 
-  delete(id: any): Observable<any> {
+  deleteUser(id: any): Observable<any> {
     return this.http.delete(`${baseUrl}/${id}`);
   }
   
