d9efe1bf28983848bc374500d072b9d7c9380c2b
cswebdev <csnider91@live.com>
2023-09-26 22:40:02 -0400

    created auth service for login, updated homecomponent.html and homecomponent.ts to send form information to backend for login validation through api call

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  userLoginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService) {
+      this.userLoginForm = this.fb.group({
+        username:['',Validators.required],
+        password:['', Validators.required]
+      })
+   
+  }
+
+  onLogin() {
+    const { username, password } = this.userLoginForm.value;
+
+    this.authService.login(username, password).subscribe(
+      response => {
+        if (response.status === 200) {
+          console.log('Successfully logged in!', response.body);
+          // You can now navigate to another route, set user details in a store, etc.
+        }
+      },
+      error => {
+        console.error('Login failed!', error);
+      }
+    );
+  }
 }
