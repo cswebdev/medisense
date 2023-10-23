28dfb1cab73606bf5474bf690d042cb78779c9a7
cswebdev <csnider91@live.com>
2023-10-23 16:29:29 -0400

    results are now parsed by name and dosage

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { MedicationService } from '../services/medication.service';
+import { AuthService } from '../services/auth.service';
+import { Medication } from '../models/medication.model';
+
 
 @Component({
   selector: 'app-search-bar',
@@ -13,34 +15,109 @@ export class SearchBarComponent {
 
   searchTerm: string = '';
   results: any[] = [];
-  constructor(private medicationService: MedicationService) {}
+  
+  searching = false;
+  focusOnList = true;
+
+  dosages: (string | null)[] = [];
+
+
+  constructor(public authService: AuthService, private medicationService: MedicationService) {}
+
+  // Method called when input field loses focus
+  onInputBlur() {
+    this.searching = false;
+
+    setTimeout(() => {
+      if (!this.focusOnList) {
+        this.showResults = false;
+      }
+    }, 100);
+  }
+
+  // Method called when input field gets focus
+  onInputFocus() {
+    this.searching = true;
+  }
+
+  // Method called when mouse is out of the search container
+  onMouseOut() {
+    this.focusOnList = false;
+
+    // Add a delay to ensure dropdown doesn't disappear immediately
+    // when moving between the input and dropdown or within dropdown items.
+    setTimeout(() => {
+      if (!this.searching) {
+        this.showResults = false;
+      }
+    }, 10000);
+  }
 
   shortenMedicationName(name: string): string {
-    const parts = name.split('/');
-    return parts[0].trim();
+    const dosage = this.getDosage(name);
+    const primaryName = name.split(' ')[0]; // Usually, the first word is the primary name
+
+    if (dosage) {
+        return `${primaryName} ${dosage}`;
+    }
+
+    return primaryName;
 }
+
+  getDosage(name: string): string | null {
+    name = name.trim();
+    const pattern = /(\d+(\.\d+)?\s*(mg|g|mcg|µg|ml|mL|L|U|IU|mEq|%))/i;
+    const match = name.match(pattern);
+    console.log("Match result for", name, ":", match);
+    return match ? match[0] : null;
+}
+
 
 
   searchMedication() {
     console.log(this.searchTerm);
     this.medicationService.searchMedicationNames(this.searchTerm).subscribe(data => {
       const extractedNames = [];
-      this.showResults = this.results.length > 0 
+      const extractedDosages = [];
+
+      this.showResults = this.results.length > 0;
 
       const conceptGroup = data.drugGroup.conceptGroup || [];
       for (let group of conceptGroup) {
         if (group.conceptProperties) {
           for (let prop of group.conceptProperties) {
-            extractedNames.push(this.shortenMedicationName(prop.name))
+            if (!prop.name.startsWith('{')) {
+              const shortenedName = this.shortenMedicationName(prop.name);
+              const dosage = this.getDosage(prop.name);
+
+              extractedNames.push(shortenedName);
+              extractedDosages.push(dosage);
+            }
           }
         }
       }
       this.results = extractedNames;
+      this.dosages = extractedDosages;
       console.log("Extracted medication names:", this.results);
+
     }, error => {
       console.error("There was an error making this request:", error)
     });
   }
 
+  onSearchTermChange() {
+    if (!this.searchTerm.trim()) {
+      this.results = [];
+     
+    }
+  }
 
+
+  addMedication(result: string, index: number) {
+    const primaryName = result.split(' ')[0]; 
+    
+    console.log("Selected medication:", primaryName);
+    console.log("Dosage for the medication:", this.dosages[index]);
+  }
+  
 }
