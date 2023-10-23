7c52960b01dc331a7d69f55cf91e7ac5adc3d469
cswebdev <csnider91@live.com>
2023-10-23 16:55:47 -0400

    removed unused imports

--- src/main/java/com/medisense/backend/models/User.java
+++ src/main/java/com/medisense/backend/models/User.java
@@ -1,17 +1,9 @@
 package com.medisense.backend.models;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
