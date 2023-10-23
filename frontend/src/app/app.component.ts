503d20972a10fbfaa451a2972cdff1093ee6fabe
MattTuccillo <matthew.tuccillo96@gmail.com>
2023-10-22 22:41:09 -0400

    added favicon and changed title

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -16,23 +15,24 @@ export class AppComponent implements OnDestroy {
 
 
   constructor(private authService: AuthService, 
-              private router: Router, 
-              private modalService: NgbModal) {
-  }
+    private modalService: NgbModal) {
+}
 
-  ngOnInit() {
-    this.authSubscription = this.authService.isAuthenticated().subscribe((user) => {
-      if (user) {
-        console.log('User is logged in');
-        // Perform any actions you need when the user is logged in
-        // this.router.navigate(['/patient-portal']); // Redirect to dashboard or any other route you want
-      } else {
-        console.log('User is logged out');
-        // Perform any actions you need when the user is logged out
-        // this.router.navigate(['/home']); // Redirect to login or any other route you want
-      }
-    });
-  }
+
+ngOnInit() {
+  this.authSubscription = this.authService.isAuthenticated().subscribe((user) => {
+    if (user) {
+      console.log('User is logged in');
+      // Perform any actions you need when the user is logged in
+      // this.router.navigate(['/patient-portal']); // Redirect to dashboard or any other route you want
+    } else {
+      console.log('User is logged out');
+      // Perform any actions you need when the user is logged out
+      // this.router.navigate(['/home']); // Redirect to login or any other route you want
+    }
+  });
+}
+
 
   ngOnDestroy() {
     // Unsubscribe from the authState subscription when the component is destroyed
