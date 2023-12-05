052d405d1d640468ace1c300e9971e25895cc27b
cswebdev <csnider91@live.com>
2023-12-05 03:07:47 -0500

    created a mvn wrapper and a chat service

---
+++ frontend/src/app/services/chat.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private chatContentSubject = new BehaviorSubject<string>(''); 
+  
+  getChatContent() {
+    return this.chatContentSubject.asObservable();
+  }
+
+  updateChatContent(content:string){
+    this.chatContentSubject.next(content);
+  }
+}
